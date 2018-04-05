619efeccd4d59935ed5a3334849e672e862ece30
Thibaut de Hollain <thibaut.dehollain@gmail.com>
2018-04-05 16:06:02 +0200

    resized columns

--- views/src/components/VehicleList.jsx
+++ views/src/components/VehicleList.jsx
@@ -87,7 +87,7 @@ export default class VehicleList extends Component {
 			<Table 
 				className='vehicleList' 
 				numRows={this.props.vehicleList.length} 
-				columnWidths={[290, 70, 70, 100, 100, 100, 80, 110, 105, 105, 80, 90, 30]}
+				columnWidths={[280, 70, 70, 100, 100, 100, 80, 110, 105, 105, 90, 90, 30]}
 				rowHeights={rowHeights}
 				enableColumnResizing={false}>
 				<Column className='tile' name='Title' cellRenderer={cellRendererTitle} />
