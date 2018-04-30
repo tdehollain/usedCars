10a7e809c075f0e87da0c7017333139a589fe2e9
Thibaut de Hollain <thibaut.dehollain@gmail.com>
2018-04-30 16:22:47 +0200

    removed logs

--- scrapper/scrapper.js
+++ scrapper/scrapper.js
@@ -56,9 +56,7 @@ async function processVehicles(vehicleData) {
 				if(nbResults > 400) {
 					console.log(`More than 20 pages. Querying by year.`);
 					// do it year by year
-					console.log(`regTo: ${vehicle.regTo}`);
 					let lastYear = vehicle.regTo || (new Date()).getFullYear();
-					console.log(`lastYear: ${lastYear}`);
 					for(i=parseInt(vehicle.regFrom, 10); i <= lastYear; i++) {
 						console.log(`   Processing: ${vehicle.title} - year ${i}`);
 						let vehicleByYear = vehicle;
