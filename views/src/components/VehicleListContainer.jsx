741c7e061a60291aea0232bfee84677ffabf2607
Thibaut de Hollain <thibaut.dehollain@gmail.com>
2018-05-10 13:44:58 +0200

    reduced bundle size

--- views/src/components/VehicleListContainer.jsx
+++ views/src/components/VehicleListContainer.jsx
@@ -30,9 +30,18 @@ class VehicleListContainer extends Component {
 	}
 
 	render() {
+		
+		let sortedList = this.props.vehicleList.sort((a,b) => {
+			if (a.title < b.title)
+				return -1;
+			if (a.title > b.title)
+				return 1;
+			return 0;
+		});
+
 		return (
 			<VehicleList 
-				vehicleList={this.props.vehicleList}
+				vehicleList={sortedList}
 				deleteVehicle={this.deleteVehicle}
 			/>
 		)
