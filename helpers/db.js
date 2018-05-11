b9e9397996175ce47715dc548a1500c0982090c4
Thibaut de Hollain <thibaut.dehollain@gmail.com>
2018-05-11 14:32:46 +0200

    debugging latestDate

--- helpers/db.js
+++ helpers/db.js
@@ -92,8 +92,7 @@ module.exports = function(mongoose){
 		let data = await vehicleModel.find({ title: title }).sort({ measureDate: 1 });
 
 		// get distinct measure dates
-		console.log(`All dates: ${data.map(el => el.measureDate).sort().reverse().getMonth()}`);
-		let latestDate = data.map(el => el.measureDate).sort().reverse()[0];
+		let latestDate = data.map(el => el.measureDate).sort((a,b) => new Date(b) - new Date(a))[0];
 		let dateThreshold = new Date(latestDate.getFullYear(), latestDate.getMonth(), latestDate.getDate());
 		let latestData = data.filter(el => el.measureDate > dateThreshold);
 		console.log(`latestDate: ${latestDate}`);
