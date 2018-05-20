bc2c5f4670859e94951d29622fcf4806012a04d6
Thibaut de Hollain <thibaut.dehollain@gmail.com>
2018-05-20 14:27:35 +0200

    added count in evolution chart

--- views/src/components/EvolutionView.jsx
+++ views/src/components/EvolutionView.jsx
@@ -3,111 +3,115 @@ import VehicleSelectionForm from './VehicleSelectionForm';
 import EvolutionChartsView from './EvolutionChartsView';
 
 export default class EvolutionView extends Component {
+  constructor() {
+    super();
 
-	constructor() {
-		super();
+    this.state = {
+      vehicleList: [],
+      selectedVehicle: '',
+      selectedVehicleURL: '',
+      vehicleData: []
+    };
 
-		this.state = {
-			vehicleList: [],
-			selectedVehicle: '',
-			selectedVehicleURL: '',
-			vehicleData: []
-		}
+    this.changeSelectedVehicle = this.changeSelectedVehicle.bind(this);
+  }
 
-		this.changeSelectedVehicle = this.changeSelectedVehicle.bind(this);
-	}
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextState.selectedVehicle === this.state.selectedVehicle &&
+      nextState.vehicleList.length === this.state.vehicleList.length &&
+      nextState.vehicleData.length === this.state.vehicleData.length
+    ) {
+      return false;
+    } else {
+      // if(nextState.vehicleList.length !== this.state.vehicleList.length) console.log('Update due to: vehicleList');
+      // if(nextState.selectedVehicle !== this.state.selectedVehicle) console.log('Update due to: selectedVehicle');
+      // if(nextState.vehicleData.length !== this.state.vehicleData.length) console.log('Update due to: vehicleData');
+      return true;
+    }
+  }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		if(
-			nextState.selectedVehicle === this.state.selectedVehicle && 
-			nextState.vehicleList.length === this.state.vehicleList.length &&
-			nextState.vehicleData.length === this.state.vehicleData.length
-		) {
-			return false;
-		} else {
-			// if(nextState.vehicleList.length !== this.state.vehicleList.length) console.log('Update due to: vehicleList');
-			// if(nextState.selectedVehicle !== this.state.selectedVehicle) console.log('Update due to: selectedVehicle');
-			// if(nextState.vehicleData.length !== this.state.vehicleData.length) console.log('Update due to: vehicleData');
-			return true;
-		}
-	}
+  componentDidMount() {
+    fetch('/api/getVehicleList')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          vehicleList: res.vehicleList
+        });
+        let selectedVehicle = localStorage.getItem('selectedVehicle');
+        if (!selectedVehicle) selectedVehicle = '';
+        this.updateSelectedVehicle(selectedVehicle);
+      });
+  }
 
-	componentDidMount() {
-		fetch('/api/getVehicleList')
-		.then(res => res.json())
-		.then(res => {
-			this.setState({
-				vehicleList: res.vehicleList
-			});
-			let selectedVehicle = localStorage.getItem('selectedVehicle');
-			if(!selectedVehicle) selectedVehicle = '';
-			this.updateSelectedVehicle(selectedVehicle);
-		});
-	}
+  changeSelectedVehicle(e) {
+    let selectedVehicle = e.target.value;
+    this.updateSelectedVehicle(selectedVehicle);
+    localStorage.setItem('selectedVehicle', selectedVehicle);
+  }
 
-	changeSelectedVehicle(e) {
-		let selectedVehicle = e.target.value;
-		this.updateSelectedVehicle(selectedVehicle);
-		localStorage.setItem('selectedVehicle', selectedVehicle);
-	}
+  updateSelectedVehicle(selectedVehicle) {
+    if (selectedVehicle === '') {
+      this.setState({
+        selectedVehicle: '',
+        selectedVehicleURL: '',
+        vehicleData: []
+      });
+    } else {
+      // get vehicle data
+      this.updateVehicleData(selectedVehicle);
+      // get selected vehicle URL
+      for (let el of this.state.vehicleList) {
+        if (el.title === selectedVehicle) {
+          this.setState({
+            selectedVehicle: selectedVehicle,
+            selectedVehicleURL: el.vehicleURL
+          });
+          break;
+        }
+      }
+    }
+  }
 
-	updateSelectedVehicle(selectedVehicle) {
-		if(selectedVehicle === '') {
-			this.setState({ selectedVehicle: '', selectedVehicleURL: '', vehicleData: [] });
-		} else {
-			// get vehicle data
-			this.updateVehicleData(selectedVehicle);
-			// get selected vehicle URL
-			for(let el of this.state.vehicleList) {
-				if(el.title === selectedVehicle) {
-					this.setState({ selectedVehicle: selectedVehicle, selectedVehicleURL: el.vehicleURL });
-					break;
-				}
-			}
-		}
-	}
+  updateVehicleData(title) {
+    // console.log('fetching vehicle data');
+    fetch('/api/getAllVehicleData', {
+      method: 'POST',
+      body: JSON.stringify({ title }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        let vehicleData = res.data;
+        this.setState({
+          vehicleData: vehicleData
+        });
+      });
+  }
 
-	updateVehicleData(title) {
-		// console.log('fetching vehicle data');
-		fetch('/api/getAllVehicleData', {
-			'method': 'POST',
-			'body': JSON.stringify({ title }),
-			"headers": {
-				"Accept": "application/json",
-				"Content-Type": "application/json"
-			}
-		})
-		.then(res => res.json())
-		.then(res => {
-			let vehicleData = res.data;
-			this.setState({
-				vehicleData: vehicleData
-			});
-		});
-	}
+  render() {
+    let sortedList = this.state.vehicleList.sort((a, b) => {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      return 0;
+    });
 
-	render() {
-		let sortedList = this.state.vehicleList.sort((a,b) => {
-			if (a.title < b.title)
-				return -1;
-			if (a.title > b.title)
-				return 1;
-			return 0;
-		});
-
-		return (
-			<div className='evolutionView'>
-				<VehicleSelectionForm 
-					vehicleList={sortedList}
-					selectedVehicle={this.state.selectedVehicle}
-					changeSelectedVehicle={this.changeSelectedVehicle}
-				/>
-				<EvolutionChartsView
-					selectedVehicle={this.state.selectedVehicle}
-					selectedVehicleURL={this.state.selectedVehicleURL}
-					vehicleData={this.state.vehicleData}
-				/>
-			</div>
-		);
-	}
+    return (
+      <div className="evolutionView">
+        <VehicleSelectionForm
+          vehicleList={sortedList}
+          selectedVehicle={this.state.selectedVehicle}
+          changeSelectedVehicle={this.changeSelectedVehicle}
+        />
+        <EvolutionChartsView
+          selectedVehicle={this.state.selectedVehicle}
+          selectedVehicleURL={this.state.selectedVehicleURL}
+          vehicleData={this.state.vehicleData}
+        />
+      </div>
+    );
+  }
 }
--- views/src/components/KmDistribution.jsx
+++ views/src/components/KmDistribution.jsx
@@ -4,106 +4,121 @@ import math from 'mathjs';
 import { getBinData, groupBy } from '../lib/mathUtil';
 
 export default class KmDistribution extends Component {
-	render() {
-		let kmBinsData = getBinData(this.props.data.map(el => el.km || 0), this.props.nbins).data;
-		let kmBinsCounts = groupBy(kmBinsData, 'binFrom', 'count');
-		let barChartData = [];
-		for(let key in kmBinsCounts) {
-			barChartData.push({ binFrom: key, count: kmBinsCounts[key]});
-		}
-		
-		// add price to kmBinsCount
-		let kmBinsPriceData = [];
-		for(let dataPoint of this.props.data) {
-			for(let key in kmBinsCounts) {
-				if(dataPoint.km <= key) {
-					kmBinsPriceData.push({ binFrom: key, price: dataPoint.price});
-					break;
-				}
-			}
-		}
-		let kmBinsPriceMedians = groupBy(kmBinsPriceData, 'binFrom', 'median', 'price');
-		
-		let lineChartData = [];
-		for(let key in kmBinsPriceMedians) {
-			lineChartData.push({ binFrom: key, median: kmBinsPriceMedians[key]});
-		}
-		let minLineChart = lineChartData.length ? math.min(lineChartData.map(el => el.binFrom)) : math.min(this.props.data.map(el => el.price));
-		let maxLineChart = lineChartData.length ? math.max(lineChartData.map(el => el.binFrom)) : math.max(this.props.data.map(el => el.price));
+  render() {
+    let kmBinsData = getBinData(
+      this.props.data.map(el => el.km || 0),
+      this.props.nbins
+    ).data;
+    let kmBinsCounts = groupBy(kmBinsData, 'binFrom', 'count');
+    let barChartData = [];
+    for (let key in kmBinsCounts) {
+      barChartData.push({ binFrom: key, count: kmBinsCounts[key] });
+    }
 
-		let fontColor = this.props.fontColor;
-		let barColor = this.props.barColor;
-		let lineColor = this.props.lineColor;
+    // add price to kmBinsCount
+    let kmBinsPriceData = [];
+    for (let dataPoint of this.props.data) {
+      for (let key in kmBinsCounts) {
+        if (dataPoint.km <= key) {
+          kmBinsPriceData.push({ binFrom: key, price: dataPoint.price });
+          break;
+        }
+      }
+    }
+    let kmBinsPriceMedians = groupBy(
+      kmBinsPriceData,
+      'binFrom',
+      'median',
+      'price'
+    );
 
-		return (
-			<Plot 
-				data = {[
-					{
-						type: 'bar',
-						x: barChartData.map(el => el.binFrom),
-						y: barChartData.map(el => el.count),
-						marker: { 
-							color: barColor
-						},
-						hoverinfo: 'y',
-						hoverlabel: {
-							bgcolor: '#293742'
-						},
-						name: 'count'
-					},
-					{
-						type: 'scatter',
-						mode: 'lines+markers',
-						x: barChartData.map(el => el.binFrom),
-						y: lineChartData.map(el => el.median),
-						marker: { 
-							color: lineColor,
-						},
-						// hoverinfo: 'skip',
-						// hoverlabel: {
-						// 	bgcolor: '#293742'
-						// },
-						yaxis: 'y2',
-						name: 'price'
-					}
-				]}
-				layout = {{
-					title: 'Mileage distribution',
-					titlefont: {
-						color: fontColor
-					},
-					width: 1200,
-					height: 400,
-					margin: { pad: 5 },
-					paper_bgcolor: 'rgba(0,0,0,0)',
-					plot_bgcolor: 'rgba(0,0,0,0)',
-					xaxis: {
-						title: 'Mileage (km)',
-						color: fontColor,
-						rangemode: 'tozero',
-						zeroline: false,
-						range: [-1*(maxLineChart-minLineChart)*0.07, maxLineChart*1.07]
-					},
-					yaxis: {
-						title: 'count',
-						color: fontColor,
-						gridcolor: '#394B59'
-					},
-					yaxis2: {
-						title: 'price (€)',
-						color: fontColor,
-						rangemode: 'tozero',
-						showgrid: false,
-						side: 'right',
-						gridcolor: '#394B59'
-					},
-					showlegend: false,
-					hovermode: 'closest'
-				}}
-				config = {{
-					displayModeBar: false
-				}}
-			/>
-		)
-	}
+    let lineChartData = [];
+    for (let key in kmBinsPriceMedians) {
+      lineChartData.push({ binFrom: key, median: kmBinsPriceMedians[key] });
+    }
+    let minLineChart = lineChartData.length
+      ? math.min(lineChartData.map(el => el.binFrom))
+      : math.min(this.props.data.map(el => el.price));
+    let maxLineChart = lineChartData.length
+      ? math.max(lineChartData.map(el => el.binFrom))
+      : math.max(this.props.data.map(el => el.price));
+
+    let fontColor = this.props.fontColor;
+    let barColor = this.props.barColor;
+    let lineColor = this.props.lineColor;
+
+    return (
+      <Plot
+        data={[
+          {
+            type: 'bar',
+            x: barChartData.map(el => el.binFrom),
+            y: barChartData.map(el => el.count),
+            marker: {
+              color: barColor
+            },
+            hoverinfo: 'y',
+            hoverlabel: {
+              bgcolor: '#293742'
+            },
+            name: 'count'
+          },
+          {
+            type: 'scatter',
+            mode: 'lines+markers',
+            x: barChartData.map(el => el.binFrom),
+            y: lineChartData.map(el => el.median),
+            marker: {
+              color: lineColor
+            },
+            // hoverinfo: 'skip',
+            // hoverlabel: {
+            // 	bgcolor: '#293742'
+            // },
+            yaxis: 'y2',
+            name: 'price'
+          }
+        ]}
+        layout={{
+          title: 'Mileage distribution',
+          titlefont: {
+            color: fontColor
+          },
+          width: 1200,
+          height: 400,
+          margin: { pad: 5 },
+          paper_bgcolor: 'rgba(0,0,0,0)',
+          plot_bgcolor: 'rgba(0,0,0,0)',
+          xaxis: {
+            title: 'Mileage (km)',
+            color: fontColor,
+            rangemode: 'tozero',
+            zeroline: false,
+            range: [
+              -1 * (maxLineChart - minLineChart) * 0.07,
+              maxLineChart * 1.07
+            ]
+          },
+          yaxis: {
+            title: 'count',
+            color: fontColor,
+            gridcolor: '#394B59'
+          },
+          yaxis2: {
+            title: 'price (€)',
+            color: fontColor,
+            rangemode: 'tozero',
+            showgrid: false,
+            side: 'right',
+            gridcolor: '#394B59'
+          },
+          showlegend: false,
+          hovermode: 'closest'
+        }}
+        config={{
+          displayModeBar: false
+        }}
+      />
+    );
+  }
 }
